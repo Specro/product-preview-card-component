4bb055a243fec87b15c59e4daf443fdaf8c9b13a
Liudas Dzisevicius <liudas.dzisevicius@gmail.com>
2024-11-28 10:15:48 +0200

    Add component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
         serif: ['Fraunces', 'serif']
+      },
+      colors: {
+        'dark-cyan': '#3C8067',
+        'dark-teal': '#1a4031',
+        'cream': '#F2EBE3',
+        'very-dark-blue': '#1C232B',
+        'dark-grayish-blue': '#6C7289'
+      },
+      fontSize: {
+        '3xl': '2rem'
+      },
+      letterSpacing: {
+        widest: '4.5px'
+      },
+      maxWidth: {
+        card: '600px'
       }
     },
   },
